ee5f4982d3705c37df024c5d2cf44fd1fe238264
raolss <44369522+raolss@users.noreply.github.com>
2026-03-17 20:07:05 +0100

    Add async setup and unload for car connectivity
    Implement async setup and unload entry functions for integration.

--- custom_components/ha_car_connectivity/__init__.py
+++ custom_components/ha_car_connectivity/__init__.py
@@ -4,6 +4,30 @@ from homeassistant.core import HomeAssistant
 from .coordinator import VWCoordinator
 from .const import DOMAIN
 
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+from carconnectivity.carconnectivity import CarConnectivity
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    config = entry.data
+
+    # Kör blocking init i executor
+    cc = await hass.async_add_executor_job(
+        CarConnectivity,
+        config
+    )
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = cc
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
+
 PLATFORMS = ["sensor"]
 
 
