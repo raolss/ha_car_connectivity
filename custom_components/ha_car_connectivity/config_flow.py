d0da0881406d497a7a8a65554cd8715b96ac9493
raolss <44369522+raolss@users.noreply.github.com>
2026-03-17 21:30:16 +0100

    Update config_flow.py

--- custom_components/ha_car_connectivity/config_flow.py
+++ custom_components/ha_car_connectivity/config_flow.py
@@ -4,28 +4,20 @@ from homeassistant import config_entries
 
 from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
 
-STEP_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_USERNAME): str,
-        vol.Required(CONF_PASSWORD): str,
-    }
-)
-
-
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-
         if user_input is not None:
-
             return self.async_create_entry(
-                title="Volkswagen",
+                title="Car Connectivity",
                 data=user_input,
             )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=STEP_SCHEMA,
+            data_schema=vol.Schema({
+                vol.Required("username"): str,
+                vol.Required("password"): str,
+            }),
         )
