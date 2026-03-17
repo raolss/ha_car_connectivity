a9774e18838be3de0c53eefc33f614a811ce128f
raolss <44369522+raolss@users.noreply.github.com>
2026-03-17 20:18:09 +0100

    Refactor VWCoordinator to VolkswagenCoordinator

--- custom_components/ha_car_connectivity/coordinator.py
+++ custom_components/ha_car_connectivity/coordinator.py
@@ -1,4 +1,5 @@
 import logging
+import DOMAIN from .const
 
 from datetime import timedelta
 
@@ -11,52 +12,22 @@ from .const import CONF_USERNAME, CONF_PASSWORD
 
 _LOGGER = logging.getLogger(__name__)
 
-
-class VWCoordinator(DataUpdateCoordinator):
-
-    def __init__(self, hass, config):
-
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-
-        self.cc = None
-        self.vehicle = None
-
+class VolkswagenCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass, entry):
         super().__init__(
             hass,
-            _LOGGER,
+            logger=__import__(__name__),
             name="Volkswagen coordinator",
             update_interval=timedelta(minutes=5),
         )
 
+        self.entry = entry
+        self.cc = hass.data[DOMAIN][entry.entry_id]
+
     async def _async_update_data(self):
+        # Kör blocking update i executor
+        await self.hass.async_add_executor_job(self.cc.update)
 
-        if self.cc is None:
+        # Returnera data du vill exponera
+        return self.cc
 
-            config = {
-                "carConnectivity": {
-                    "connectors": [
-                        {
-                            "type": "volkswagen",
-                            "config": {
-                                "username": self.username,
-                                "password": self.password,
-                            },
-                        }
-                    ]
-                }
-            }
-
-            self.cc = carConnectivity(config)
-
-            await self.cc.start()
-
-            connector = self.cc.connectors[0]
-
-            self.vehicle = connector.vehicles[0]
-
-        await self.cc.update()
-
-        return {
-            "soc": self.vehicle.drivetrain.battery.level.value
-        }
