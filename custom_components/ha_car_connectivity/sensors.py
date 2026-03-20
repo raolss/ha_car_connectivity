570bc2cb28b79a5e7da8fa2a04daf4bdc16b10e2
raolss <44369522+raolss@users.noreply.github.com>
2026-03-20 17:29:04 +0100

    Update sensors.py

--- custom_components/ha_car_connectivity/sensors.py
+++ custom_components/ha_car_connectivity/sensors.py
@@ -4,7 +4,30 @@ from homeassistant.helpers.entity import Entity
 
 from .const import DOMAIN
 
+async def async_setup_entry(hass, entry, async_add_entities):
+    data = hass.data[DOMAIN][entry.entry_id]
+    api = data["api"]
+    vehicles = data["vehicles"]
 
+    entities = []
+
+    for vehicle in vehicles:
+        status = await api.get_vehicle_status(vehicle["vin"])
+
+        if "battery" in status:
+            entities.append(CarBatterySensor(api, vehicle))
+
+        if "range" in status:
+            entities.append(CarRangeSensor(api, vehicle))
+
+        if "doors" in status:
+            entities.append(CarDoorLockSensor(api, vehicle))
+
+    async_add_entities(entities)
+
+
+
+"""
 async def async_setup_entry(hass, entry, async_add_entities):
 
     coordinator = hass.data[DOMAIN][entry.entry_id]
@@ -22,7 +45,7 @@ class BatterySensor(SensorEntity):
         self._attr_native_unit_of_measurement = "%"
 
     @property
-    def native_value(self):
+    def native_value(self):"""
         return self.coordinator.data["soc"]
 
     async def async_update(self):
